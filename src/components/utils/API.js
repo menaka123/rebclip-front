0c400e27bbcf1bd52d77978d41f4ce113b2a3f3b
menakafernando <project.menaka@gmail.com>
2020-04-14 18:58:54 +0300

    update url

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class API
 {
-    static URL_API = 'http://127.0.0.1:8000/api/';
+    static URL_API = 'http://localhost/api/';
     static instance = null;
 
     static getInstance()
